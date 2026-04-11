0aef6646a663407a0fc5c123640a6226eb58c4fa
Your Name <you@example.com>
2026-04-11 13:16:53 +0100

    chapter 2

---
+++ chapter2.py/Arithmetic.py
@@ -0,0 +1,8 @@
+t = 27.5
+g = 2 
+print(t + g)
+print(t - g)
+print(t * g)
+print(t / g)
+print(t // g)
+print(t ** g)
---
+++ chapter2.py/multiple.py
@@ -0,0 +1,9 @@
+number = 1024
+number_two = 2
+if (number % 4 == 0):
+   print("it is a multiple of 4")
+if(number_two % 10 == 0):
+   print("it is a multiple of 2")
+
+else:
+  print("it is not multiple of 2") 
