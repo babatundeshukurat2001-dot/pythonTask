2e8b6205a66b1bb8f2e90b726f476ee132dbbcbd
Your Name <you@example.com>
2026-04-12 13:31:21 +0100

    Beans na water

---
+++ Assignment/Beans na water 2/Calculator.py
@@ -0,0 +1,7 @@
+principal = input("Enter the principal: ")
+principal = float(principal)
+rate = input("Enter the rate: ")
+rate = float(rate)
+duration = input("Enter the duration:")
+duration = int(duration)
+print("monthly payment" , principal , rate *(1 + rate)** duration/ (1 + rate)** duration - 1)
