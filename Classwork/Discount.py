2163295c97cba17d5ee5fbcb3ef339a4640b0acd
Your Name <you@example.com>
2026-04-17 19:35:07 +0100

    password.py

---
+++ Classwork/Discount.py
@@ -0,0 +1,9 @@
+The greater the the purchased amount,the higher the discount offered.
+Tis means when the user buy more he tend to get higher discount
+But when the user buy less the discount reduces 
+So the if the user purchases between 1,000 and 10,000 receive a 5% discount which is multiplied by 0.05
+if the user purchases between 10,000 and 50,000 receive a 10% discount multiply by 0.1 
+if the user purchases between 50,000 receive a 20% discount multiply by 0.2 
+so print the total amount
+
+
---
+++ Classwork/classplay.py
@@ -0,0 +1,6 @@
+name = input("what is your name: ")
+age = input("what is your age: ")
+
+age = int(age)
+
+print("your name is", name, "and your", (age +10), "years old")
